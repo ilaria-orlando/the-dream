0e430b1f603c9125fcfaa1a22f066def13bbb00d
Ilaria Orlando <ilaria.orlando1991@gmail.com>
2021-01-06 11:10:26 +0100

    add html form to calculate currency

--- index.php
+++ index.php
@@ -5,6 +5,11 @@
         <title>Currency calculator</title>
     </head>
     <body>
-
+        <h1>Currency exchange calculator</h1>
+    <form method="POST" action="index.php">
+        <label for="euro">Enter the amount:</label>
+        <input type="number" name="euro">
+        <input type="submit" value="calculate">
+    </form>
     </body>
 </html>
