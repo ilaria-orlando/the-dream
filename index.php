5b8f9cd4188a33d74de12c061ea7dfd8f649550c
Ilaria Orlando <ilaria.orlando1991@gmail.com>
2021-01-06 16:06:39 +0100

    add styling

--- index.php
+++ index.php
@@ -7,62 +7,65 @@
         <title>Currency calculator</title>
     </head>
     <body>
-        <div class="container">
-            <h1>Currency exchange calculator</h1>
+        <div class="container-sm text-center bg-light mt-3 pb-3 d-flex flex-column justify-content-center">
+            <h1 class="text-primary m-3 pb-3">Currency exchange calculator</h1>
 
         <!-- exchange rate form -->
+            <div class="card text-muted m-3 p-3 align-self-center" style="width: 400px;">
+                <form method="POST" action="index.php" class="p-2">
+                    <label for="exchange" class="p-1">Enter the amount:</label><br>
+                    <input type="number" name="exchange" class="form-control p-1"><br>
+                    <label for="currency" class="p-1">Select currency:</label><br>
+                    <select name="currency" class="form-select m-1">
+                        <option value="NZD">NZ dollar</option>
+                        <option value="GPB">British pound</option>
+                        <option value="USD">U.S. dollar</option>
+                    </select><br>
+                    <input name="toEuro" type="submit" value="To Euro" class="btn btn-primary m-1">
+                    <input name="fromEuro" type="submit" value="From Euro" class="btn btn-primary m-1">
+                </form>
 
-            <form method="POST" action="index.php">
-                <label for="exchange">Enter the amount:</label><br>
-                <input type="number" name="exchange"><br>
-                <label for="currency">Select currency:</label><br>
-                <select name="currency">
-                    <option value="NZD">NZ dollar</option>
-                    <option value="GPB">British pound</option>
-                    <option value="USD">U.S. dollar</option>
-                </select><br>
-                <input name="toEuro" type="submit" value="To Euro">
-                <input name="fromEuro" type="submit" value="From Euro">
-            </form>
+            <!-- php calculation -->
+                <?php
+                    //check if submit button is pressed, from selected currency to euro
+                    if(isset($_POST['toEuro'])){
+                        //take the amount entered and calculate it to euros
+                        if($_POST['currency'] == 'NZD'){
+                            $amount = $_POST['exchange'] * 0.58;
+                        }
+                        if($_POST['currency'] == 'GPB'){
+                            $amount = $_POST['exchange'] * 1.1057;
+                        }
+                        if($_POST['currency'] == 'USD'){
+                            $amount = $_POST['exchange'] * 0.8109;
+                        }
+                        //
 
-        <!-- php calculation -->
-            <?php
-                //check if submit button is pressed, from selected currency to euro
-                if(isset($_POST['toEuro'])){
-                    //take the amount entered and calculate it to euros
-                    if($_POST['currency'] == 'NZD'){
-                        $amount = $_POST['exchange'] * 0.58;
+                        $rounded = number_format($amount, 2);
+                        echo "{$_POST['exchange']} {$_POST['currency']} is {$rounded} EUR";
                     }
-                    if($_POST['currency'] == 'GPB'){
-                        $amount = $_POST['exchange'] * 1.1057;
-                    }
-                    if($_POST['currency'] == 'USD'){
-                        $amount = $_POST['exchange'] * 0.8109;
-                    }
-                    //
-                    echo "{$_POST['exchange']} {$_POST['currency']} is {$amount} EUR";
-                }
 
-                //check if submit button is pressed, from euro to selected currency
-                if(isset($_POST['fromEuro'])){
-                    //take the amount entered and calculate it to selected currency
-                    if($_POST['currency'] == 'NZD'){
-                        $amount = $_POST['exchange'] * 1.6899;
+                    //check if submit button is pressed, from euro to selected currency
+                    if(isset($_POST['fromEuro'])){
+                        //take the amount entered and calculate it to selected currency
+                        if($_POST['currency'] == 'NZD'){
+                            $amount = $_POST['exchange'] * 1.6899;
+                        }
+                        if($_POST['currency'] == 'GPB'){
+                            $amount = $_POST['exchange'] * 0.9044;
+                        }
+                        if($_POST['currency'] == 'USD'){
+                            $amount = $_POST['exchange'] * 1.2332;
+                        }
+                        //
+                        echo "{$_POST['exchange']} EUR is {$amount} {$_POST['currency']}";
                     }
-                    if($_POST['currency'] == 'GPB'){
-                        $amount = $_POST['exchange'] * 0.9044;
-                    }
-                    if($_POST['currency'] == 'USD'){
-                        $amount = $_POST['exchange'] * 1.2332;
-                    }
-                    //
-                    echo "{$_POST['exchange']} EUR is {$amount} {$_POST['currency']}";
-                }
 
-            ini_set('display_errors', '1');
-            ini_set('display_startup_errors', '1');
-            error_reporting(E_ALL);
-            ?>
+                ini_set('display_errors', '1');
+                ini_set('display_startup_errors', '1');
+                error_reporting(E_ALL);
+                ?>
+            </div>
         </div>
     </body>
 </html>
