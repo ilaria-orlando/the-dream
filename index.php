638fef7cc08b3d0489aabfc56dba0724fd9d9ea9
Ilaria Orlando <ilaria.orlando1991@gmail.com>
2021-01-06 11:30:56 +0100

    add php submit

--- index.php
+++ index.php
@@ -5,11 +5,24 @@
         <title>Currency calculator</title>
     </head>
     <body>
-        <h1>Currency exchange calculator</h1>
-    <form method="POST" action="index.php">
-        <label for="euro">Enter the amount:</label>
-        <input type="number" name="euro">
-        <input type="submit" value="calculate">
-    </form>
+        <h1>Currency exchange calculator for New Zealand dollar</h1>
+
+        <!-- exchange rate form -->
+
+        <form method="POST" action="index.php">
+            <label for="euro">Enter the amount:</label>
+            <input type="number" name="nzdollar">
+            <input name="submit" type="submit" value="calculate">
+        </form>
+
+        <!-- php calculation -->
+            <?php
+                if(isset($_POST['submit'])){
+                    echo "it works!";
+                }
+            ini_set('display_errors', '1');
+            ini_set('display_startup_errors', '1');
+            error_reporting(E_ALL);
+            ?>
     </body>
 </html>
